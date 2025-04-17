8696d3dec4f5289ea8e416175b42946825a92866
Seba <sebastiantorresola3@gmail.com>
2025-04-17 15:49:39 -0700

    Dummy api request fetching and fixed content security policy

--- electron-client/src/renderer/src/App.tsx
+++ electron-client/src/renderer/src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react'
+
 function App(): JSX.Element {
+  const [response, setResponse] = useState(null)
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch('http://localhost:5001')
+      const data = await res.json()
+      setResponse(data)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+    console.log('data fetching')
+  }, [])
+
   return (
     <>
       <h1>Hello, Discord Clone!</h1>
+      {response ? (
+        <div>
+          <h2>Response from server:</h2>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
 }
